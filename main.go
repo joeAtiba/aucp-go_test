7f4c6b9aaf4a5e2318143a10aa05a2df0a0d34d8
joeAtiba <107277733+joeAtiba@users.noreply.github.com>
2022-06-24 11:20:11 -0400

    Remove "now" text.

--- main.go
+++ main.go
@@ -24,11 +24,11 @@ type iMac struct {
 }
 
 func (phone iPhone) turnOn() string {
-	return "iOS now starting up..."
+	return "iOS starting up..."
 }
 
 func (mac iMac) turnOn() string {
-	return "macOS now starting up..."
+	return "macOS starting up..."
 }
 
 func (phone *iPhone) update(v float32) {
