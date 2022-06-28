66fa6ddda5f413b5f977052c55376b9586dc6a60
Joe Baltimore <joebalt@skypeak.com>
2022-06-28 15:30:09 -0400

    testing pipelines

--- main.go
+++ main.go
@@ -24,11 +24,11 @@ type iMac struct {
 }
 
 func (phone iPhone) turnOn() string {
-	return "iOS is starting up..."
+	return "iOS is now starting up..."
 }
 
 func (mac iMac) turnOn() string {
-	return "macOS is starting up..."
+	return "macOS is now starting up..."
 }
 
 func (phone *iPhone) update(v float32) {
